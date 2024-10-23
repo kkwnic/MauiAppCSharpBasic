064ea8a83266bbdf4ebc3f5b27458b533fccdaab
Nicha Buranatana <nicha@nichas-macbook.local>
2024-10-23 16:31:15 +0700

    first commit

---
+++ MauiAppCSharpBasic1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppCSharpBasic1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiAppCSharpBasic1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppCSharpBasic1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
